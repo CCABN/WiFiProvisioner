64d00a0271a7a6a47a15c528006ef090d9c8c82d
tallis-larsen <tallisblarsen@gmail.com>
2025-09-22 22:08:55 -0500

    ok one more time

--- src/html_template.h
+++ src/html_template.h
@@ -3,14 +3,14 @@
 
 #include <Arduino.h>
 
-// Macro to include file content as raw string literal
-#define INCLUDE_HTML_FILE(filepath) \
-    const char HTML_CONTENT[] PROGMEM = R"HTMLDELIM(" \
-    #include filepath \
-    ")HTMLDELIM";
+// Stringify macro to convert file content to string
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
 
-// Include the HTML file from data directory
-INCLUDE_HTML_FILE("../data/index.html")
+// Raw string literal containing the HTML file content
+const char HTML_CONTENT[] PROGMEM =
+#include "../data/index.html"
+;
 
 inline String getHTMLTemplate() {
     return String(FPSTR(HTML_CONTENT));
