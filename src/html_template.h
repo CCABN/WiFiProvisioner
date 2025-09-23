db938e35fdb39ce135617b7439fc685fb768b6b5
tallis-larsen <tallisblarsen@gmail.com>
2025-09-22 22:02:23 -0500

    nevermind it has compile errors we gonna try again hahahahhaahahahahahahahhhhhahahahahahah
    approaching 5 hour time limit and this is too advanced for me to want to learn rn so please for the love of god work ahhhh

--- src/html_template.h
+++ src/html_template.h
@@ -3,17 +3,17 @@
 
 #include <Arduino.h>
 
-// GCC/Clang builtin to include file as binary data
-// This creates a symbol that points to the file contents
-extern "C" {
-    extern const uint8_t index_html_start[] asm("_binary_data_index_html_start");
-    extern const uint8_t index_html_end[] asm("_binary_data_index_html_end");
-}
+// Macro to include file content as raw string literal
+#define INCLUDE_HTML_FILE(filepath) \
+    const char HTML_CONTENT[] PROGMEM = R"HTMLDELIM(" \
+    #include filepath \
+    ")HTMLDELIM";
 
-// Runtime conversion function
+// Include the HTML file from data directory
+INCLUDE_HTML_FILE("../data/index.html")
+
 inline String getHTMLTemplate() {
-    size_t html_size = index_html_end - index_html_start;
-    return String((const char*)index_html_start, html_size);
+    return String(FPSTR(HTML_CONTENT));
 }
 
 #endif // HTML_TEMPLATE_H
